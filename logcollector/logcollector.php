bb5deeb1d9de507483b40231a061d4be8e3825c6
jxmot <webdev@stupiddog.net>
2021-01-25 04:31:42 -0600

    initial commit, logcollector.php

---
+++ logcollector/logcollector.php
@@ -0,0 +1,31 @@
+<?php
+require_once './timezone.php';
+require_once './rightnow.php';  // modified
+
+$accnt = json_decode(file_get_contents('./imapaccnt.json'));
+$imapsrv = "{$accnt->host}:{$accnt->port}/imap/ssl";
+
+$mailbox = imap_open("{{$imapsrv}}INBOX", $accnt->login, $accnt->pword,  OP_READONLY );
+if($mailbox === false) {
+    echo rightnow('log') . " - logcollector.php: Failed to connect to server\n";
+    exit(0);
+}
+
+$msgqty = imap_num_msg($mailbox); 
+if($msgqty === false) {
+    echo rightnow('log') . " - logcollector.php: error calling imap_num_msg\n";
+    exit(0);
+} else if($msgqty === 0) {
+    echo rightnow('log') . " - logcollector.php: no messages\n";
+    exit(0);
+}
+
+define('_IMAPSRV',$imapsrv);
+define('_MAILBOX',$mailbox);
+define('_MSGQTY',$msgqty);
+
+require_once './msgmgr.php';
+
+imap_close(_MAILBOX);
+exit(0);
+?>
