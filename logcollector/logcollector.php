476bdb899bcb9d48efb4bf7735dc7a85aa6951e0
jxmot <webdev@stupiddog.net>
2021-01-25 04:42:17 -0600

    switch to dev json

--- logcollector/logcollector.php
+++ logcollector/logcollector.php
@@ -2,7 +2,7 @@
 require_once './timezone.php';
 require_once './rightnow.php';  // modified
 
-$accnt = json_decode(file_get_contents('./imapaccnt.json'));
+$accnt = json_decode(file_get_contents('./_imapaccnt.json'));
 $imapsrv = "{$accnt->host}:{$accnt->port}/imap/ssl";
 
 $mailbox = imap_open("{{$imapsrv}}INBOX", $accnt->login, $accnt->pword,  OP_READONLY );
@@ -27,5 +27,6 @@ define('_MSGQTY',$msgqty);
 require_once './msgmgr.php';
 
 imap_close(_MAILBOX);
+echo rightnow('log') . " - logcollector.php: complete, normal exit\n";
 exit(0);
 ?>
