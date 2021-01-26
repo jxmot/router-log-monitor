1a8960a641f060bea6b1a46d7cc31b67fabfa6a8
jxmot <webdev@stupiddog.net>
2021-01-25 22:49:32 -0600

    SQL code, wip

--- sql/initall-rlmon.sql
+++ sql/initall-rlmon.sql
@@ -2,35 +2,73 @@
 create schema rlmonitor;
 use rlmonitor;
 
--- A
+create table known (
+    id integer(5) auto_increment not null,
+    primary key(id),
+    ip varchar(40) default null,
+    mac varchar(20) default null,
+);
+
+create table ipstats (
+    ip varchar(40) not null, 
+    primary key(ip),
+    known boolean default false,
+    actioncounts text default '[0,0,0,0,0,0,0,0,0,0,0]' 
+);
+
+------------------------------------------------------
+------------------------------------------------------
+
+create table logentry (
+    tstamp bigint(16) not null, primary key (tstamp) 
+    actiontype integer(3) not null,             -- enumerated actions
+
+    ip varchar(40) default null,
+    port varchar(6) default null,
+    srcip varchar(40) default null,
+    srcport varchar(6) default null
+    host varchar(128) default null,
+    mac varchar(20) default null,
+    message varchar(128) default null
+);
+
+
+--------------------------------------------------------
+
+--                VS.
+
+--------------------------------------------------------
+
+-- 
 create table actions (
     tstamp bigint(16) not null, primary key (tstamp) 
-    type integer(3) not null,                    -- enumerated actions
+    actiontype integer(3) not null,                    -- enumerated actions
     ip varchar(40) default null,
     host varchar(128) default null,
     message varchar(128) default null
 );
 
+-- A
 create table invasions (
     tstamp bigint(16) not null, primary key (tstamp) 
-    type integer(3) not null,                    -- enumerated actions
+    actiontype integer(3) not null,                    -- enumerated actions
     ip varchar(40) default null,
-    mac varchar(128) default null,
+    mac varchar(20) default null,
     message varchar(128) default null
 );
 
 -- A
 create table lanactivity (
     tstamp bigint(16) not null, primary key (tstamp) 
-    type integer(3) not null,                    -- enumerated actions
+    actiontype integer(3) not null,                    -- enumerated actions
     ip varchar(40) default null,
-    host varchar(128) default null,
-    message varchar(128) default null
+    mac varchar(20) default null--,
+--    message varchar(128) default null
 );
 
 create table netactivity (
     tstamp bigint(16) not null, primary key (tstamp) 
-    type integer(3) not null,                    -- enumerated actions
+    actiontype integer(3) not null,                    -- enumerated actions
     ip varchar(40) default null,
     port varchar(6) default null,
     srcip varchar(40) default null,
@@ -39,8 +77,7 @@ create table netactivity (
 
 create table updates (
     tstamp bigint(16) not null, primary key (tstamp) 
-    type integer(3) not null,                    -- enumerated actions
-    ip varchar(40) default null,
+    actiontype integer(3) not null,                    -- enumerated actions
     message varchar(128) default null
 );
 
