4b14317c320da00d298f7e5a57789c58551ffdb5
jxmot <webdev@stupiddog.net>
2021-01-28 14:49:47 -0600

    WIP: able to extract date/time from log entry and convert to unix time

--- logwatcher/logdata.js
+++ logwatcher/logdata.js
@@ -7,6 +7,10 @@ module.exports = (function(pevts, _log)  {
     logdata = {
     };
 
+    // needed for fs.watch(), fs.statSync(), and
+    // fs.accessSync()
+    var fs = require('fs');
+
     // set up run-time logging
     var path = require('path');
     var scriptName = path.basename(__filename);
@@ -35,10 +39,66 @@ module.exports = (function(pevts, _log)  {
         } else {
             // parse the log data and write to database...
             log(`- process(): ${wfile.path}${wfile.filename}`);
+            logToDB(wfile);
             // announce completion...
             pevts.emit('LOG_PROCESSED', wfile);
         }
         return dbopen;
+    };
+
+    function logToDB(wfile) {
+        // make sure the file is valid
+        if(wfile !== undefined) {
+            try {
+                fs.accessSync(`${wfile.path}${wfile.filename}`, fs.constants.F_OK);
+            } catch(err) {
+                if(err.code === 'ENOENT') {
+                    log(`logToDB(): does not exist: ${wfile.path}${wfile.filename}`);
+                    // emit error?
+                }
+                return;
+            }
+            // valid, open and read it 
+            // https://nodejs.org/docs/latest-v12.x/api/fs.html#fs_fs_opensync_path_flags_mode
+            var fd = fs.openSync(wfile.path+wfile.filename, 'r');
+            var buff = Buffer.alloc(wfile.size);
+            var rdqty = fs.readSync(fd, buff);
+            fs.closeSync(fd);
+            if(rdqty === wfile.size) {
+                // body string to array of lines
+                var logstr = buff.toString();
+                var logarr = logstr.split("\r\n");
+                // interate through array of lines - 
+                //      parse each line into object
+                //      write object to db
+                //      next line
+                logarr.forEach((entry, idx) => {
+                    parseEntry(entry, idx);
+                });
+    
+                // send LOG_PROCESSED
+            }
+        } else {
+            log(`logToDB(): undefined - wfile`);
+        }
+    };
+
+    // [Internet connected] IP address: 73.176.4.88, Friday, Jan 22,2021 17:51:54
+    function parseEntry(_entry, idx) {
+        var tmp    = _entry.replace("\n",'')
+        var entry  = tmp.replace("\r",'')
+        var entarr = entry.split(' ');
+        /*
+            length minus:
+                -5 = 73.176.4.88,
+                -4 = Friday,
+                -3 = Jan
+                -2 = 22,2021
+                -1 = 17:51:54
+        */
+        var todstr = `${entarr[entarr.length - 3]} ${entarr[entarr.length - 2].replace(',',', ')} ${entarr[entarr.length - 1]}`;
+        var tstamp = new Date(todstr).getTime();
+        log(`parseEntry(): ${todstr} ${tstamp}`);
     };
 
     return logdata;
