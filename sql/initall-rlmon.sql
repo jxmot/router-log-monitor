839667f686cce22936a99093f1dd512628150cfe
jxmot <webdev@stupiddog.net>
2021-01-28 12:37:24 -0600

    added action related tables, fixed typos

--- sql/initall-rlmon.sql
+++ sql/initall-rlmon.sql
@@ -2,7 +2,7 @@
 create schema rlmonitor;
 use rlmonitor;
 
-create table known (
+create table rlmonitor.known (
     id integer(5) auto_increment not null,
     primary key(id),
     ip varchar(40) default null,
@@ -13,25 +13,42 @@ create table known (
     catcode integer(4) default 0
 );
 
-create table ipstats (
+/*
+*/
+create table rlmonitor.ipstats (
     ip varchar(40) not null, 
     primary key(ip),
     mac varchar(20) default null,
     known boolean default false,
-    actioncounts text default '[0,0,0,0,0,0,0,0,0,0,0]' 
+    actioncounts varchar(64) default '[0,0,0,0,0,0,0,0,0,0,0]',
+    lastcount bigint(16) not null,
+    tstamp bigint(16) default null,
+    ipid integer(5) not null
+);
+
+create table rlmonitor.actioncats (
+    catid integer(4) not null,
+    catcode varchar(3) not null,
+    description varchar(64) not null
+);
+
+create table rlmonitor.actions (
+    actionid integer(4) not null,
+    description varchar(64) not null,
+    catcode varchar(3) not null
 );
 
 ------------------------------------------------------
 ------------------------------------------------------
 
-create table logentry (
-    tstamp bigint(16) not null, primary key (tstamp) 
-    actiontype integer(3) not null,             -- enumerated actions
-
+create table rlmonitor.logentry (
+    tstamp bigint(16) not null, 
+    primary key (tstamp),
+    actionid integer(3) not null,             -- enumerated actions
     ip varchar(40) default null,
     port varchar(6) default null,
     srcip varchar(40) default null,
-    srcport varchar(6) default null
+    srcport varchar(6) default null,
     host varchar(128) default null,
     mac varchar(20) default null,
     message varchar(128) default null
