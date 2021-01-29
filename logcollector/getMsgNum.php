b9459987be3c555ca596042c366e550a68e3cca5
jxmot <webdev@stupiddog.net>
2021-01-29 15:55:23 -0600

    removed old commetns

--- logcollector/getMsgNum.php
+++ logcollector/getMsgNum.php
@@ -13,13 +13,9 @@ function getMsgNum($hdr) {
 $msgnum = -1;
     // also checks for "U"nread
     if(preg_match("/[U].+[0-9]\)/", $hdr)) {
-        //$tmp = preg_split("/[ ].+[0-9]\)/", $headers[$head_idx]);
         $sp1 = preg_split("/\)/", $hdr);
-        //    print_r($sp1);
         $sp2 = preg_split("/[A-Z]/", $sp1[0]);
-    //    print_r($sp2);
         $msgnum = intval($sp2[1]);
-    //    echo "\nmsg # = {$msgnum}\n\n\n";
     }
     return $msgnum;
 }
