a0afc81fb2fe6817dcd2003ff89e89dd1a86be7c
jxmot <webdev@stupiddog.net>
2021-01-30 01:51:09 -0600

    comments, minor changes

--- logcollector/msgmgr.php
+++ logcollector/msgmgr.php
@@ -6,15 +6,15 @@
 require_once('./getMsgNum.php');
 require_once('./isLogMsg.php');
 require_once('./logProcess.php');
+
 // safety check, make sure we are connected
 if(!defined('_IMAPSRV') || !defined('_MAILBOX')) {
     echo rightnow('log') . " - msgmgr.php: not connected\n";
     exit(0);
 }
 
-// TODO: investigate(trial-n-error) where the expunge
-// should be done
-// get rid of any messages that were marked for deletion
+// if configured, get rid of any messages that were 
+// marked for deletion before we read them.
 if((defined('_READONLY') && _READONLY === false) &&
    (defined('_EXPUNGE') && _EXPUNGE === true) &&
    (defined('_EXPWHERE') && _EXPWHERE === 'readbegin')) {
@@ -25,7 +25,7 @@ if((defined('_READONLY') && _READONLY === false) &&
 // like this - 
 //      U       1)23-Jan-2021 Jim                  NETGEAR R6400 Log [C6:C0: (24621 chars)
 $headers = imap_headers(_MAILBOX);
-if ($headers === false) {
+if($headers === false) {
     echo rightnow('log') . " - msgmgr.php: Failed to list headers in mailbox\n";
     exit(0);
 }
@@ -41,27 +41,31 @@ foreach($headers as $hdr) {
             logProcess($num);
             if(defined('_READONLY') &&  _READONLY === false) {
                 if(defined('_MSGDISPOSE') &&  _MSGDISPOSE === 'seen') {
-                    // TODO: investigate if this is where this should be done
                     imap_setflag_full(_MAILBOX, $num, "\\Seen");
-                        echo rightnow('log') . " - num = {$num} has been SEEN\n";
+                    echo rightnow('log') . " - num = {$num} marked as SEEN\n";
                 } else { //      OR 
                     if(defined('_MSGDISPOSE') &&  _MSGDISPOSE === 'delete') {
                         imap_delete(_MAILBOX,$num);
-                        echo rightnow('log') . " - num = {$num} has been DELETED\n";
+                        echo rightnow('log') . " - num = {$num} marked as DELETED\n";
                     }
                 }
             }
-            // TODO: delay, don't blast through the messages.
-            // 
+            // TODO: delay, don't blast through the messages(?)
+            // REALITY Check: For example, if there are 1000 messages
+            // to read and there is a 30 second delay betweeh each
+            // then it will take about 8 hours to read them all. But
+            // if the delay was 5 seconds then it would take 1 to 
+            // 2 hours.
+            // NOTE: Under normal "operating conditions" we would 
+            // only be reading 1 message at a time, as it is received.
+            // Reading bulk is only happening because there are messages
+            // to catch up on.
         }
     } else {
-        echo rightnow('log') . " - msgmgr.php: getMsgNum() not UNread [{$hdr}]\n";
-//        exit(0);
+        echo rightnow('log') . " - msgmgr.php: getMsgNum() not Unread [{$hdr}]\n";
     }
 }
-// TODO: investigate(trial-n-error) where the expunge
-// should be done.
-// 
+// Is expungement enabled?
 if((defined('_READONLY') && _READONLY === false) &&
    (defined('_EXPUNGE') && _EXPUNGE === true) &&
    (defined('_EXPWHERE') && _EXPWHERE === 'readend')) {
