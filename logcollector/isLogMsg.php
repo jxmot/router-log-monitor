213ba030cb280f80a025e64407efc034548db030
jxmot <webdev@stupiddog.net>
2021-01-30 15:12:59 -0600

    add bool flag to args, true = delete unwanted msgs. clean up, improved

--- logcollector/isLogMsg.php
+++ logcollector/isLogMsg.php
@@ -12,23 +12,17 @@
 */
 define('_LOGSUBJ', 'NETGEAR R6400 Log');
 
-function isLogMsg($msgnum) {
+function isLogMsg($msgnum, $rmv = false) {
 $ret = false;
-
     // https://www.php.net/manual/en/function.imap-headerinfo.php
     $header = imap_headerinfo(_MAILBOX,$msgnum);
-    //echo "sent = {$header->MailDate}\n";
-    //echo "date = {$header->date}\n\n\n";
-    //echo "sent  = " . strtotime($header->MailDate) . "\n";
-    //echo "date  = " . strtotime($header->date) . "\n";
-    //echo "udate = {$header->udate}\n\n";
-    //echo rightnow('name',$header->MailDate) . "\n\n\n";
-    //echo "subject = {$header->subject}\n\n\n";
-    if(strpos($header->subject, _LOGSUBJ) === false) {
-        $ret = false;
-        imap_delete(_MAILBOX,$msgnum);
-    } else {
-        $ret = $header;
+    if(($header->Deleted !== 'D') && ($header->Unseen === 'U')){
+        if(strpos($header->subject, _LOGSUBJ) === false) {
+            $ret = false;
+            if($rmv === true) imap_delete(_MAILBOX,$msgnum);
+        } else {
+            $ret = $header;
+        }
     }
     return $ret;
 }
