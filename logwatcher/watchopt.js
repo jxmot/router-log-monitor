3b6c3fb8407927eb161f54cc7673667fc5767646
jxmot <webdev@stupiddog.net>
2021-02-02 15:07:29 -0600

    added properties that are used for managing "bad" log entries

--- logwatcher/watchopt.js
+++ logwatcher/watchopt.js
@@ -1,4 +1,7 @@
 // options and settings for logwatch.js
 module.exports = {
-    path : './../logoutput/'
+    path : './../logoutput/',
+    movebad : true,
+    mintstamp: 1523771032000,
+    delbad: true
 };
