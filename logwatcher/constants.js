db72158a7162532e3e44e707f7358fb7c0a2cf6b
jxmot <webdev@stupiddog.net>
2021-02-10 02:47:48 -0600

    combined DOS_FIN and DOS_ACK into DOS_ATT

--- logwatcher/constants.js
+++ logwatcher/constants.js
@@ -13,18 +13,17 @@ Implemented so far:
 
 |actionid|description                      |catcode|
 |--------|---------------------------------|-------|
-|12      |UPnP set event                   |NA     |
-|9       |LAN access from remote           |NA     |
+|11      |UPnP set event                   |NA     |
+|8       |LAN access from remote           |NA     |
 |1       |Admin login                      |RA     |
-|5       |Dynamic DNS                      |RA     |
-|7       |Internet connected               |RA     |
-|8       |Internet disconnected            |RA     |
-|10      |Time synchronized with NTP server|RA     |
-|3       |DoS attack: FIN Scan             |RI     |
-|4       |DoS attack: ACK Scan             |RI     |
-|11      |WLAN access rejected             |RI     |
+|4       |Dynamic DNS                      |RA     |
+|6       |Internet connected               |RA     |
+|7       |Internet disconnected            |RA     |
+|9       |Time synchronized with NTP server|RA     |
+|3       |DoS attack                       |RI     |
+|10      |WLAN access rejected             |RI     |
 |2       |DHCP IP                          |RL     |
-|6       |Initialized, firmware version    |RU     |
+|5       |Initialized, firmware version    |RU     |
 
 
 Full List(?):
@@ -34,7 +33,7 @@ Full List(?):
 |Admin login                      |Y           |       |
 |DHCP IP                          |Y           |       |
 |Dynamic DNS                      |Y           |       |
-|DoS attack                       |Y x 2       |       |
+|DoS attack                       |Y           |       |
 |email failed                     |            |RA     |
 |email sent to                    |            |RA     |
 |Initialized                      |Y           |       |
@@ -62,16 +61,15 @@ module.exports = {
     RI         : 'RI',  // Router Invasion    
     RL         : 'RL',  // Router LAN Activity
     RU         : 'RU',  // Router Updates     
-    UPNP_EVENT : 12,    // NA
-    LAN_ACC    : 9,     // NA
+    UPNP_EVENT : 11,    // NA
+    LAN_ACC    : 8,     // NA
     ADM_LOG    : 1,     // RA
-    DYN_DNS    : 5,     // RA
-    INET_CONN  : 7,     // RA
-    INET_DCONN : 8,     // RA
-    TIME_SYNC  : 10,    // RA
-    DOS_FIN    : 3,     // RI
-    DOS_ACK    : 4,     // RI
-    WLAN_REJ   : 11,    // RI
+    DYN_DNS    : 4,     // RA
+    INET_CONN  : 6,     // RA
+    INET_DCONN : 7,     // RA
+    TIME_SYNC  : 9,     // RA
+    DOS_ATT    : 3,     // RI
+    WLAN_REJ   : 10,    // RI
     DHCP_IP    : 2,     // RL
-    FIRMW_UP   : 6      // RU
+    FIRMW_UP   : 5      // RU
 };
