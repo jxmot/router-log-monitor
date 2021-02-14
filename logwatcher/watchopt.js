e5c515d949b3795467d4691c4a052bb3f5796d48
jxmot <webdev@stupiddog.net>
2021-02-14 11:59:41 -0600

    added comments, and new options for handling used log files

--- logwatcher/watchopt.js
+++ logwatcher/watchopt.js
@@ -1,12 +1,25 @@
-// options and settings for logwatch.js
+// options and settings for logwatch.js and logread.js
 module.exports = {
     path : './../logoutput/',
+    // log file names are: YYYYMMDD-HHMMSS-net.log
+    nameregexp: /^\d{8}-\d{6}-net\.log/g,
     movebad : true,
     mintstamp: 1523771032000,
     delbad: true,
     // used in logread.js, readdel has 
-    // priority over readren
+    // priority over readmov and readren,
+    // and readmov has priority over readren
     readdel: false,
-    readren: true,
+    readmov: true,
+    // NOTE: this is realative to "path", so things
+    // like "../folder/" will work. If the path does
+    // not exist it will be created. This will be
+    // added to 'path'.
+    movpath: 'oldlogs/',
+    // rename the file(s), renchar can be a string
+    // instead of a single character. 
+    readren: false,
+    renchar: '_',
+    // only used in logread.js
     readexit: true
 };
