a3383cf753f9467f860d020b9cb5d25d74301623
jxmot <webdev@stupiddog.net>
2021-04-24 11:03:05 -0500

    moved onDatabaseError() and openDB(), improved passing args to modules
    also added report module

--- logwatcher/rlmonitor.js
+++ logwatcher/rlmonitor.js
@@ -59,13 +59,37 @@ function openDone(dbopen, errObj) {
     // did we have success?
     if(dbopen === false) {
         // no, log errors and end the transaction
-        log(`- ERROR : openDone() errObj = ${JSON.stringify(errObj)}`);
+        log(`openDone() - ERROR : errObj = ${JSON.stringify(errObj)}`);
     } else {
         // do some database stuff
-        log('- openDone() - success! ready for some database stuff');
+        log('openDone() - success! ready for some database stuff');
     }
     procs_evts.emit('DB_OPEN', {state:dbopen,db:(dbopen === true ? database : errObj)});
 };
+
+// handle database errors here, including when the server closes 
+// the connection after a period of being "idle".
+function onDatabaseError(err) {
+    log(`onDatabaseError() err = ${err}`);
+    if(err.message.includes('The server closed the connection') === true) {
+        procs_evts.emit('DB_CLOSED', {state:false,db:null});
+        log(`onDatabaseError() sent DB_CLOSED, reopening database...`);
+        setTimeout(openDB,2500); 
+    } else {
+        console.log("*******************************\n");
+        console.log(`onDatabaseError() err = ${err}`);
+        console.log("*******************************\n");
+        process.exit(1);
+    }
+};
+
+// Open the database
+// NOTE: MySQL will keep an inactive connection 
+// open for only 8 hours. Then it will close it.
+function openDB() {
+    database.openDB(openDone, onDatabaseError);
+};
+
 
 //var fopt = process.argv[2];
 //var fopt = 'watch';
@@ -82,39 +106,41 @@ if(typeof fopt !== 'undefined') {
             break;
 
         default:
-            log(`- ERROR unknown operation ${fopt}`);
+            log(`ERROR unknown operation ${fopt}`);
             exit(0);
             break;
     };
 } else {
     const watcher = require('./logwatch.js')(watch_evts, procs_evts, _log);
 }
+
+/*
+    The "app" object, it contains anything that we need to pass on to
+    a module. 
+*/
+var app = {
+    constants: require('./constants.js'),
+    staticdata: require('./staticdata.js')(procs_evts, _log),
+    _log: _log,
+    pevts: procs_evts
+}
+
 // Process the log files into the database
-const procs = require('./logprocess.js')(watch_evts, procs_evts, _log);
+
+// the log processor
+const ldata = require('./logdata.js')(app);
+
+watch_evts.on('FILE_CREATED', (watchit) => {
+    if(!logmute) log(`FILE_CREATED: ${watchit.filename} in ${watchit.path}`);
+    ldata.process(JSON.parse(JSON.stringify(watchit)));
+});
+
+// 
+watch_evts.on('FILE_DELETED', (watchit) => {
+    if(!logmute) log(`FILE_DELETED: ${watchit.filename} in ${watchit.path}`);
+});
+
 // Generate static reports
-const reports = require('./reports.js')(procs_evts, _log);
-
-// handle database errors here, including when the server closes 
-// the connection after a period of being "idle".
-function onDatabaseError(err) {
-    log(`- onDatabaseError() err = ${err}`);
-    if(err.message.includes('The server closed the connection') === true) {
-        procs_evts.emit('DB_CLOSED', {state:false,db:null});
-        log(`- onDatabaseError() sent DB_CLOSED, reopening database...`);
-        setTimeout(openDB,2500); 
-    } else {
-        console.log("*******************************\n");
-        console.log(`onDatabaseError() err = ${err}`);
-        console.log("*******************************\n");
-        process.exit(1);
-    }
-};
-
-// Open the database
-// NOTE: MySQL will keep an inactive connection 
-// open for only 8 hours. Then it will close it.
-function openDB() {
-    database.openDB(openDone, onDatabaseError);
-};
+const reports = require('./reports.js')(app);
 
 openDB();
