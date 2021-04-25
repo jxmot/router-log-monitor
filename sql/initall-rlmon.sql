9c7c74dc646753aabe4a23e3d2324d10efc72572
jxmot <webdev@stupiddog.net>
2021-04-25 15:40:55 -0500

    added rlmonitor.attacktypes, attacks, and wlanrejs

--- sql/initall-rlmon.sql
+++ sql/initall-rlmon.sql
@@ -100,6 +100,60 @@ create table rlmonitor.invasions (
 );
 
 
+-- 1 Unknown    Cannot parse attack
+-- 2 FIN Scan   FIN packet received
+-- 3 ACK Scan   ACK packet received
+-- 4 STORM      STORM Attack
+-- 5 Smurf      SMURF Attack
+create table rlmonitor.attacktypes (
+    attackid integer(4) not null,
+    attackcode varchar(32) not null,
+    description varchar(64) default null,
+);
+
+-- FIN Scan: (1) attack packets in last 20 sec from ip [151.101.65.69]
+-- ACK Scan: (1) attack packets in last 20 sec from ip [151.101.65.69]
+-- STORM: (1) attack packets in last 20 sec from ip [151.101.65.69]
+-- Smurf: (1) attack packets in last 20 sec from ip [151.101.65.69]
+
+create table rlmonitor.attacks (
+    tstamp bigint(16) not null, 
+    -- NOTE: 'unique' should prevent duplication,
+    -- since this field is copied from logentry
+    entrynumb bigint(16) unique not null,
+    -- entry data...
+    ip varchar(40) not null,
+    -- parsed from rlmonitor.logentry->message
+    attackcode varchar(40) not null,
+    attackid int(4) not null,
+    qty int(4) not null,
+    sec int(4) not null,
+    -- some IPs might be known
+    known boolean default false,
+    -- filled in with a 2nd pass, read & update
+    hostname varchar(128) default null,
+-- temporary!
+    message varchar(128) default null,
+-- temporary?
+    logfile varchar(64) default null,
+    logentry varchar(512) default null
+);
+
+
+create table rlmonitor.wlanrejs (
+    tstamp bigint(16) not null, 
+    -- NOTE: 'unique' should prevent duplication,
+    -- since this field is copied from logentry
+    entrynumb bigint(16) unique not null,
+    mac varchar(20) not null,
+    message varchar(128) not null,
+    ip varchar(40) not null,
+    -- some MACs/IPs might be known
+    known boolean default false,
+    -- temporary?
+    logfile varchar(64) default null,
+    logentry varchar(512) default null
+);
 
 --------------------------------------------------------
 
