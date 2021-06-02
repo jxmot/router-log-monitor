8a984af1cc67db7ad5e37f938e449c1822272926
jxmot <webdev@stupiddog.net>
2021-06-02 15:13:20 -0500

    initial commit, SQL statement file

---
+++ logreporter/sql/invasions-lifetime.sql
@@ -0,0 +1,16 @@
+select count(ip) as invasion_qty,
+       ip,toport,hostname, 
+       (cast(((max(tstamp) - min(tstamp)) / 1000) as UNSIGNED)) as epoch_dur, 
+       floor(((max(tstamp) - min(tstamp)) / 1000) / 86400) as day_span,
+	   SEC_TO_TIME(cast(mod(((max(tstamp) - min(tstamp)) / 1000), 86400) as UNSIGNED)) as time_span, 
+       DATE_FORMAT(FROM_UNIXTIME(min(tstamp) / 1000), '%Y-%m-%d @ %T') as first_invade,
+       min(entrynumb) as first_entry, 
+       DATE_FORMAT(FROM_UNIXTIME(max(tstamp) / 1000), '%Y-%m-%d @ %T') as last_invade,
+       max(entrynumb) as last_entry
+FROM rlmonitor.invasions where 
+known = 0 
+and 
+toport != 59018 
+group by ip 
+order by count(ip) desc;
+-- invasions-lifetime.sql : returns all invasions, by IP hits with calculated date spans
