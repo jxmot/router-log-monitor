11fb572b310eb7ef0372aab45b3a4c64447c9890
jxmot <webdev@stupiddog.net>
2021-06-02 15:09:28 -0500

    initial commit, report generator

---
+++ logreporter/reports.js
@@ -0,0 +1,67 @@
+'use strict';
+/*
+*/
+module.exports = (function(_pevts, _log)  {
+
+    const pevts = _pevts;
+
+    // disable(mute) some log() calls
+    const logmute = false;
+    // enable/disable all logging in this module
+    const logenable = true;
+    // set up run-time logging
+    const scriptName = require('path').basename(__filename);
+    function log(payload) {
+        if(logenable) _log(`${scriptName} - ${payload}`);
+    };
+
+    // https://handlebarsjs.com/
+    const hbars = require('handlebars');
+    //const template = hbars.compile('Name: {{name}}');
+
+    let reports = {
+    };
+
+    let dbopen = false;
+    let dbobj = {};
+    let dbcfg = {};
+
+    pevts.on('DB_OPEN', (_dbobj) => {
+        if(_dbobj.state === true) {
+            dbopen = true;
+            dbobj = _dbobj.db;
+            log('DB_OPEN: success');
+            dbcfg = dbobj.getDBCcfg();
+            // prepare for report requests(?)
+        } else {
+            log(`DB_OPEN: ERROR ${_dbobj.db.err.message}`);
+        }
+    });
+
+    pevts.on('DB_CLOSED', (_dbobj) => {
+        dbopen = false;
+        dbobj = {};
+        // abort all reports
+    });
+
+    pevts.on('REPORTREQ', (reportid, readResp, res) => {
+        log(`REPORTREQ: reportid = ${reportid}`);
+
+        if(dbopen = true) {
+            dbobj.runSQL('./sql/test-report.sql', (data, err) => {
+                log(`REPORTREQ: data here`);
+                if(!err) readResp(JSON.stringify(data), res);
+                else readResp(null, res);
+            });
+        }
+
+
+
+        // allow queueing?
+
+        // or isolate with settimeout?
+    });
+
+    return reports;
+});
+
