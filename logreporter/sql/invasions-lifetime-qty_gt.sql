2341cc5e8c2b75196aa80f3df809a13fe014016a
jxmot <webdev@stupiddog.net>
2021-06-02 22:26:08 -0500

    changed date&time column to separate date and time columns

--- logreporter/sql/invasions-lifetime-qty_gt.sql
+++ logreporter/sql/invasions-lifetime-qty_gt.sql
@@ -3,15 +3,18 @@ select from(
         ip,toport,hostname, 
         (cast(((max(tstamp) - min(tstamp)) / 1000) as UNSIGNED)) as epoch_dur, 
         floor(((max(tstamp) - min(tstamp)) / 1000) / 86400) as day_span,
-         SEC_TO_TIME(cast(mod(((max(tstamp) - min(tstamp)) / 1000), 86400) as UNSIGNED)) as time_span, 
-        DATE_FORMAT(FROM_UNIXTIME(min(tstamp) / 1000), '%Y-%m-%d @ %T') as first_invade,
+        SEC_TO_TIME(cast(mod(((max(tstamp) - min(tstamp)) / 1000), 86400) as UNSIGNED)) as time_span, 
+        DATE_FORMAT(FROM_UNIXTIME(min(tstamp) / 1000), '%Y-%m-%d') as first_date,
+        DATE_FORMAT(FROM_UNIXTIME(min(tstamp) / 1000), '%T') as first_time,
         min(entrynumb) as first_entry, 
-        DATE_FORMAT(FROM_UNIXTIME(max(tstamp) / 1000), '%Y-%m-%d @ %T') as last_invade,
+        DATE_FORMAT(FROM_UNIXTIME(max(tstamp) / 1000), '%Y-%m-%d') as last_date,
+        DATE_FORMAT(FROM_UNIXTIME(max(tstamp) / 1000), '%T') as last_time,
         max(entrynumb) as last_entry
     FROM rlmonitor.invasions where 
     known = 0 
     and 
     toport != 59018 
+-- where mod
     group by ip 
     order by count(ip) desc;
 ) as tempdata 
--- logreporter/sql/invasions-lifetime.sql
+++ logreporter/sql/invasions-lifetime.sql
@@ -3,14 +3,17 @@ select count(ip) as invasion_qty,
        (cast(((max(tstamp) - min(tstamp)) / 1000) as UNSIGNED)) as epoch_dur, 
        floor(((max(tstamp) - min(tstamp)) / 1000) / 86400) as day_span,
 	   SEC_TO_TIME(cast(mod(((max(tstamp) - min(tstamp)) / 1000), 86400) as UNSIGNED)) as time_span, 
-       DATE_FORMAT(FROM_UNIXTIME(min(tstamp) / 1000), '%Y-%m-%d @ %T') as first_invade,
+       DATE_FORMAT(FROM_UNIXTIME(min(tstamp) / 1000), '%Y-%m-%d') as first_date,
+       DATE_FORMAT(FROM_UNIXTIME(min(tstamp) / 1000), '%T') as first_time,
        min(entrynumb) as first_entry, 
-       DATE_FORMAT(FROM_UNIXTIME(max(tstamp) / 1000), '%Y-%m-%d @ %T') as last_invade,
+       DATE_FORMAT(FROM_UNIXTIME(max(tstamp) / 1000), '%Y-%m-%d') as last_date,
+       DATE_FORMAT(FROM_UNIXTIME(max(tstamp) / 1000), '%T') as last_time,
        max(entrynumb) as last_entry
 FROM rlmonitor.invasions where 
 known = 0 
 and 
 toport != 59018 
+-- where mod
 group by ip 
 order by count(ip) desc;
 -- invasions-lifetime.sql : returns all invasions, by IP hits with calculated date spans
@@ -22,8 +25,8 @@ order by count(ip) desc;
 --        "epoch_dur": 204,
 --        "day_span": 0,
 --        "time_span": "00:03:24",
---        "first_invade": "2018-10-09 @ 10:04:58",
+--        "first_event": "2018-10-09 @ 10:04:58",
 --        "first_entry": 8671,
---        "last_invade": "2018-10-09 @ 10:08:22",
+--        "last_event": "2018-10-09 @ 10:08:22",
 --        "last_entry": 8849
 --      }
