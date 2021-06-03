dbe1a1675b35954f00af49ab45dd5e863d28448c
jxmot <webdev@stupiddog.net>
2021-06-03 00:47:43 -0500

    initial commit, report table definitions

---
+++ logreporter/reportdefs.js
@@ -0,0 +1,40 @@
+'use strict';
+
+module.exports = {
+    rdefs: {
+        'invasions-lifetime' : {
+            th: {
+                'invasion_qty':'Total',
+                'ip':'IP Addr',
+                'toport':'Port #',
+                'hostname':'Host Name',
+                // empty column heading, do not display
+                'epoch_dur':'',
+                'day_span':'Days',
+                'time_span':'HH:MM:SS',
+                'first_date':'Date',
+                'first_time':'Time',
+                'first_entry':'',
+                'last_date':'Date',
+                'last_time':'Time',
+                'last_entry':''                    
+            }
+        },
+        'wlanrejects-lifetime': {
+            th: {
+                'wlanrej_qty':'Total',
+                'mac':'MAC Addr',
+                'macmfr':'Made By',
+                'epoch_dur':'',
+                'day_span':'Days',
+                'time_span':'HH:MM:SS',
+                'first_date':'Date',
+                'first_time':'Time',
+                'first_entry':'',
+                'last_date':'Date',
+                'last_time':'Time',
+                'last_entry':''                   
+            }
+        }
+    }
+};
