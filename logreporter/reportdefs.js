c041ca48db2941e3b8a37585eb710007ac9d1be3
jxmot <webdev@stupiddog.net>
2021-06-03 01:16:05 -0500

    added report definitions

--- logreporter/reportdefs.js
+++ logreporter/reportdefs.js
@@ -17,7 +17,41 @@ module.exports = {
                 'first_entry':'',
                 'last_date':'Date',
                 'last_time':'Time',
-                'last_entry':''                    
+                'last_entry':''
+            }
+        },
+        'invasions-lifetime-qty_gt' : {
+            th: {
+                'invasion_qty':'Total',
+                'ip':'IP Addr',
+                'toport':'Port #',
+                'hostname':'Host Name',
+                // empty column heading, do not display
+                'epoch_dur':'',
+                'day_span':'Days',
+                'time_span':'HH:MM:SS',
+                'first_date':'Date',
+                'first_time':'Time',
+                'first_entry':'',
+                'last_date':'Date',
+                'last_time':'Time',
+                'last_entry':''
+            }
+        },
+        'invasions-lifetime-by_port' : {
+            th: {
+                'invasion_qty':'Total',
+                'toport':'Port #',
+                // empty column heading, do not display
+                'epoch_dur':'',
+                'day_span':'Days',
+                'time_span':'HH:MM:SS',
+                'first_date':'Date',
+                'first_time':'Time',
+                'first_entry':'',
+                'last_date':'Date',
+                'last_time':'Time',
+                'last_entry':''
             }
         },
         'wlanrejects-lifetime': {
